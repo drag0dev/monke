ebb2fcb3d84f2359c5877b31256b746f39c0597c
Dragoslav Tamindžija <dragodev@protonmail.com>
2024-04-06 18:48:52 +0200

    feat: implement let statement parsing

--- parser/parser.go
+++ parser/parser.go
@@ -28,5 +28,57 @@ func (p *Parser) NextToken() {
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
-    return nil
+    program := &ast.Program{}
+    program.Statements = []ast.Statement{}
+
+    for p.currToken.Type != token.EOF {
+        stmt := p.parseStatement()
+        if stmt != nil {
+            program.Statements = append(program.Statements, stmt)
+        }
+        p.NextToken()
+    }
+
+    return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+    switch p.currToken.Type {
+    case token.LET:
+        return p.parseLetStatement()
+    default:
+        return nil
+    }
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+    stmt := &ast.LetStatement{Token: p.currToken}
+
+    if !p.expectPeek(token.IDENT) {
+        return nil
+    }
+
+    stmt.Name = &ast.Identifier{Token: p.currToken, Value: p.currToken.Literal}
+
+    if !p.expectPeek(token.ASSIGN) {
+        return nil
+    }
+
+    // TODO: were skipping the expressions until we encounter a semicolon
+    for !p.currTokenIs(token.SEMICOLON) {
+        p.NextToken()
+    }
+
+    return stmt
+}
+
+func (p *Parser) currTokenIs(t token.TokenType) bool { return p.currToken.Type == t }
+func (p *Parser) peekTokenIs(t token.TokenType) bool { return p.peekToken.Type == t }
+func (p *Parser) expectPeek(t token.TokenType) bool {
+    if p.peekTokenIs(t) {
+        p.NextToken()
+        return true
+    } else {
+        return false
+    }
 }
